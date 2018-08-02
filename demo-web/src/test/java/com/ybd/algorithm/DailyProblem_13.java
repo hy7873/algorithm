520e6c0bd0269ddb276d1858578a45090abe8823
wanghai <wangh_2@sina.com>
2018-08-02 22:48:41 +0800

    配置

---
+++ demo-web/src/test/java/com/ybd/algorithm/DailyProblem_13.java
@@ -0,0 +1,29 @@
+package com.ybd.algorithm;
+
+import org.junit.Test;
+
+/**
+ *
+ Good morning! Here's your coding interview problem for today.
+
+ This problem was asked by Amazon.
+
+ Given an integer k and a string s, find the length of the longest substring that contains at most k distinct characters.
+
+ For example, given s = "abcba" and k = 2, the longest substring with k distinct characters is "bcb".
+
+ Upgrade to premium and get in-depth solutions to every problem.
+
+ If you liked this problem, feel free to forward it along so they can subscribe here! As always, shoot us an email if there's anything we can help with!
+ */
+public class DailyProblem_13 {
+
+
+    @Test
+    public void test1() {
+        System.out.println("test1");
+    }
+
+
+
+}
