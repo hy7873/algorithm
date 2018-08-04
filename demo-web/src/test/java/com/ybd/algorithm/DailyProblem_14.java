48abc85aad92f1db8defe9a4823267f2915deb12
wanghai <wangh_2@sina.com>
2018-08-04 23:27:07 +0800

    15

---
+++ demo-web/src/test/java/com/ybd/algorithm/DailyProblem_14.java
@@ -0,0 +1,41 @@
+package com.ybd.algorithm;
+
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ *
+ This problem was asked by Google.
+
+ The area of a circle is defined as πr^2. Estimate π to 3 decimal places using a Monte Carlo method.
+
+ Hint: The basic equation of a circle is x2 + y2 = r2.
+
+ We will be sending the solution tomorrow, along with tomorrow's question. As always, feel free to shoot us an email if there's anything we can help with.
+ */
+public class DailyProblem_14 {
+
+
+    @Test
+    public void test1() {
+        int num = 100000000;
+        double distance = 0;
+        int hitPot = 0;
+        for (int i = 0;i < num;i++) {
+            double x = 2 * Math.random();
+            double y = 2 * Math.random();
+            //System.out.println("(" + x + "," + y +")");
+            distance = Math.sqrt(Math.pow((x - 1),2) + Math.pow((y - 1),2));
+            if (distance < 1) {
+                hitPot ++;
+            }
+        }
+        BigDecimal b = new BigDecimal(hitPot).divide(new BigDecimal(num),8,BigDecimal.ROUND_DOWN).multiply(new BigDecimal(4));
+        double PI = b.doubleValue();
+        System.out.println( "hitPot = " +hitPot + "  PI = " + PI);
+    }
+
+}
