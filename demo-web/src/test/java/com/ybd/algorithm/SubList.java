d267c5aa86057527cc15389987d5d9b80537d39e
wanghai <wangh_2@sina.com>
2018-10-02 12:02:11 +0800

    子集

---
+++ demo-web/src/test/java/com/ybd/algorithm/SubList.java
@@ -0,0 +1,43 @@
+package com.ybd.algorithm;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by Administrator on 2018/10/2.
+ * 求一个集合的子集
+ */
+public class SubList {
+
+    public static List<List<Integer>> getSubset(List<Integer> L) {
+        List<List<Integer>> lists = new ArrayList<>();
+        if (L == null || L.size() == 0) {
+            return null;
+        }
+        for (int i = 0; i < Math.pow(2,L.size()); i++) {
+            List<Integer> list = new ArrayList<>();
+            int index = i;
+            for (int j = 0; j < L.size();j++) {
+                if ((index & 1) == 1){
+                    list.add(L.get(j));
+                }
+                index >>= 1;
+            }
+            System.out.println(list);
+            lists.add(list);
+        }
+        return lists;
+    }
+
+    @Test
+    public void test() {
+        List<Integer> L = Arrays.asList(2,3,5,6,7);
+        getSubset(L);
+    }
+
+
+
+}
