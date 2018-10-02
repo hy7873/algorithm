ccee7066a4a609663d48837246ceefc61ecd89b1
wanghai <wangh_2@sina.com>
2018-10-02 12:08:37 +0800

    子集

--- demo-web/src/test/java/com/ybd/algorithm/SubList.java
+++ demo-web/src/test/java/com/ybd/algorithm/SubList.java
@@ -39,5 +39,15 @@ public class SubList {
     }
 
 
+    @Test
+    public void test1() {
+        for (int i = 1; i <= 1000; i++) {
+            if ((i & (i - 1)) == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+
+
 
 }
