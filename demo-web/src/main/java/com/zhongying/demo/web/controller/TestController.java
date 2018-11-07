3a8c3ced78660c1edf4e1676e0d78a9496c7afbd
wanghai <wangh_2@sina.com>
2018-11-07 21:55:51 +0800

    jetty

--- demo-web/src/main/java/com/zhongying/demo/web/controller/TestController.java
+++ demo-web/src/main/java/com/zhongying/demo/web/controller/TestController.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Api(description = "测试")
 @Controller
@@ -15,6 +16,10 @@ public class TestController {
 			.getLogger(TestController.class);
 
 
+	@RequestMapping(value = "test1",method = RequestMethod.POST)
+	public void test1() {
+		System.out.println("test1");
+	}
 
 
 }
