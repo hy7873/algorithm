753af96297471a28a787e140bb38b9d6d73a7306
wanghai <wangh_2@sina.com>
2018-11-15 16:08:07 +0800

    快速排序

--- demo-web/src/test/java/com/ybd/service/TestMain.java
+++ demo-web/src/test/java/com/ybd/service/TestMain.java
@@ -1,6 +1,7 @@
 package com.ybd.service;
 
 import com.zhongying.demo.algorithm.sort.BubbleSort;
+import com.zhongying.demo.algorithm.sort.QuickSort;
 import com.zhongying.demo.util.JacksonUtil;
 import org.junit.Test;
 
@@ -13,6 +14,13 @@ public class TestMain {
     public void test1() {
         int[] a = {2,3,5,1,8,-1,-9};
         a = BubbleSort.bubbleSort(a);
+        System.out.println(JacksonUtil.bean2Json(a));
+    }
+
+    @Test
+    public void test2() {
+        int[] a = {2,3,5,1,8,-1,-9};
+        QuickSort.quickSort(a,0,a.length - 1);
         System.out.println(JacksonUtil.bean2Json(a));
     }
 
