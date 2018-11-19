938ca5197efec8c7cacf69429f97ef692691dfad
wanghai <wangh_2@sina.com>
2018-11-19 20:20:20 +0800

    插入排序

--- demo-web/src/test/java/com/ybd/service/TestMain.java
+++ demo-web/src/test/java/com/ybd/service/TestMain.java
@@ -1,6 +1,7 @@
 package com.ybd.service;
 
 import com.zhongying.demo.algorithm.sort.BubbleSort;
+import com.zhongying.demo.algorithm.sort.InsertSort;
 import com.zhongying.demo.algorithm.sort.QuickSort;
 import com.zhongying.demo.util.JacksonUtil;
 import org.junit.Test;
@@ -21,6 +22,13 @@ public class TestMain {
     public void test2() {
         int[] a = {2,3,5,1,8,-1,-9};
         QuickSort.quickSort(a,0,a.length - 1);
+        System.out.println(JacksonUtil.bean2Json(a));
+    }
+
+    @Test
+    public void test3() {
+        int[] a = {2,3,5,1,8,-1,-9,17,201,-100};
+        InsertSort.insertSort2(a);
         System.out.println(JacksonUtil.bean2Json(a));
     }
 
