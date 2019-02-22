bd830715e6ea1fd6caf95ad508cdd25b103e451f
wanghai <wangh_2@sina.com>
2019-02-22 11:39:05 +0800

    稀疏数

---
+++ demo-service/src/main/java/com/hy/demo/algorithm/dailyProblem/SparseNumber.java
@@ -0,0 +1,50 @@
+package com.hy.demo.algorithm.dailyProblem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author: wanghai
+ * @Date:2019/2/22 10:46
+ * @Copyright:reach-life
+ * @Description:稀疏数
+ * 我们说一个数是稀疏的，如果在它的二进制表示中没有相邻的数。例如，21（10101）是稀疏的，但22（10110）不是。对于给定的输入n，找到大于或等于n的最小稀疏数
+ */
+public class SparseNumber {
+
+    public static int getMinSparseNumber(int n) {
+        while (true) {
+            if (isSparseNumber(n)) {
+               return n;
+            }
+            n++;
+        }
+    }
+
+    public static boolean isSparseNumber(int n) {
+        List<Integer> list = new ArrayList<>();
+        boolean flag = false;
+        for (int i = 31; i >= 0 ; i--) {
+            int k = n >>> i & 1;
+            if (k != 0 && !flag) {
+                flag = true;
+            }
+            if (flag) {
+                list.add(k);
+            }
+        }
+        System.out.println(list);
+        for (int i = 0; i < list.size() - 1; i++) {
+            if (list.get(i) == list.get( i + 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(getMinSparseNumber(4));;
+    }
+
+}
